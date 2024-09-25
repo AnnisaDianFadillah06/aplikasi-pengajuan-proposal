f63f108fdbe64d46e5e51a5e06ccd3bfb4142245
AnnisaDianFadillah06 <annisadianfadillah@gmail.com>
2024-09-25 12:48:51 +0700

    perbaikan pemanggilan asset js

--- public/js/soft-ui-dashboard-tailwind.js
+++ public/js/soft-ui-dashboard-tailwind.js
@@ -15,65 +15,142 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
+// var page = window.location.pathname.split("/").pop().split(".")[0];
+// var aux = window.location.pathname.split("/");
+// var to_build = (aux.includes('pages')?'../':'./');
+// var root = window.location.pathname.split("/")
+// if (!aux.includes("pages")) {
+//   page = "dashboard";
+// }
+
+// loadStylesheet(to_build + "assets/css/perfect-scrollbar.css");
+// loadJS(to_build + "assets/js/perfect-scrollbar.js", true);
+
+// if (document.querySelector("nav [navbar-trigger]")) {
+//   loadJS(to_build + "assets/js/navbar-collapse.js", true);
+// }
+
+// if (document.querySelector("[data-target='tooltip']")) {
+//   loadJS(to_build + "assets/js/tooltips.js", true);
+//   loadStylesheet(to_build + "assets/css/tooltips.css");
+// }
+
+// if (document.querySelector("[nav-pills]")) {
+//   loadJS(to_build + "assets/js/nav-pills.js", true);
+// }
+
+// if (document.querySelector("[dropdown-trigger]")) {
+//   loadJS(to_build + "assets/js/dropdown.js", true);
+
+// }
+
+// if (document.querySelector("[fixed-plugin]")) {
+//   loadJS(to_build + "assets/js/fixed-plugin.js", true);
+// }
+
+// if (document.querySelector("[navbar-main]")) {
+//   loadJS(to_build + "assets/js/sidenav-burger.js", true);
+//   loadJS(to_build + "assets/js/navbar-sticky.js", true);
+// }
+
+// if (document.querySelector("canvas")) {
+//   loadJS(to_build + "assets/js/chart-1.js", true);
+//   loadJS(to_build + "assets/js/chart-2.js", true);
+// }
+
+// function loadJS(FILE_URL, async) {
+//   let dynamicScript = document.createElement("script");
+
+//   dynamicScript.setAttribute("src", FILE_URL);
+//   dynamicScript.setAttribute("type", "text/javascript");
+//   dynamicScript.setAttribute("async", async);
+
+//   document.head.appendChild(dynamicScript);
+// }
+
+// function loadStylesheet(FILE_URL) {
+//   let dynamicStylesheet = document.createElement("link");
+
+//   dynamicStylesheet.setAttribute("href", FILE_URL);
+//   dynamicStylesheet.setAttribute("type", "text/css");
+//   dynamicStylesheet.setAttribute("rel", "stylesheet");
+
+//   document.head.appendChild(dynamicStylesheet);
+// }
+/*! Soft UI Dashboard Tailwind - v1.0.5 */
+
+/*! Soft UI Dashboard Tailwind - v1.0.5 */
+
+// Mendapatkan halaman saat ini
+/*! Soft UI Dashboard Tailwind - v1.0.5 */
+
+// Mendapatkan halaman saat ini
 var page = window.location.pathname.split("/").pop().split(".")[0];
 var aux = window.location.pathname.split("/");
-var to_build = (aux.includes('pages')?'../':'./');
-var root = window.location.pathname.split("/")
-if (!aux.includes("pages")) {
-  page = "dashboard";
+var to_build = aux.includes("pages") ? "../" : "./";
+var root = window.location.pathname.split("/");
+
+// Fungsi untuk memuat file JavaScript
+function loadJS(src, async = true) {
+    let script = document.createElement("script");
+    script.setAttribute("src", src);
+    script.setAttribute("type", "text/javascript");
+    script.setAttribute("async", async);
+    document.head.appendChild(script);
 }
 
-loadStylesheet(to_build + "assets/css/perfect-scrollbar.css");
-loadJS(to_build + "assets/js/perfect-scrollbar.js", true);
+// Fungsi untuk memuat file CSS
+function loadStylesheet(href) {
+    let link = document.createElement("link");
+    link.setAttribute("href", href);
+    link.setAttribute("type", "text/css");
+    link.setAttribute("rel", "stylesheet");
+    document.head.appendChild(link);
+}
 
+// Pastikan asset dimuat dengan benar
+page = aux.includes("pages") ? page : "dashboard";
+
+// Muat file CSS
+loadStylesheet('/css/perfect-scrollbar.css');
+
+// Muat file JS
+loadJS('/js/perfect-scrollbar.js');
+
+// Muat file JS navbar-collapse jika ada navbar-trigger
 if (document.querySelector("nav [navbar-trigger]")) {
-  loadJS(to_build + "assets/js/navbar-collapse.js", true);
+    loadJS('/js/navbar-collapse.js');
 }
 
+// Muat file JS dan CSS untuk tooltips jika ada data-target='tooltip'
 if (document.querySelector("[data-target='tooltip']")) {
-  loadJS(to_build + "assets/js/tooltips.js", true);
-  loadStylesheet(to_build + "assets/css/tooltips.css");
+    loadJS('/js/tooltips.js');
+    loadStylesheet('/css/tooltips.css');
 }
 
+// Muat file JS untuk nav-pills jika ada atribut nav-pills
 if (document.querySelector("[nav-pills]")) {
-  loadJS(to_build + "assets/js/nav-pills.js", true);
+    loadJS('/js/nav-pills.js');
 }
 
+// Muat file JS untuk dropdown jika ada dropdown-trigger
 if (document.querySelector("[dropdown-trigger]")) {
-  loadJS(to_build + "assets/js/dropdown.js", true);
-
+    loadJS('/js/dropdown.js');
 }
 
+// Muat file JS untuk fixed-plugin jika ada fixed-plugin
 if (document.querySelector("[fixed-plugin]")) {
-  loadJS(to_build + "assets/js/fixed-plugin.js", true);
+    loadJS('/js/fixed-plugin.js');
 }
 
+// Muat file JS untuk navbar-main jika ada navbar-main
 if (document.querySelector("[navbar-main]")) {
-  loadJS(to_build + "assets/js/sidenav-burger.js", true);
-  loadJS(to_build + "assets/js/navbar-sticky.js", true);
+    loadJS('/js/sidenav-burger.js');
+    loadJS('/js/navbar-sticky.js');
 }
 
+// Muat file JS untuk chart jika ada canvas
 if (document.querySelector("canvas")) {
-  loadJS(to_build + "assets/js/chart-1.js", true);
-  loadJS(to_build + "assets/js/chart-2.js", true);
-}
-
-function loadJS(FILE_URL, async) {
-  let dynamicScript = document.createElement("script");
-
-  dynamicScript.setAttribute("src", FILE_URL);
-  dynamicScript.setAttribute("type", "text/javascript");
-  dynamicScript.setAttribute("async", async);
-
-  document.head.appendChild(dynamicScript);
-}
-
-function loadStylesheet(FILE_URL) {
-  let dynamicStylesheet = document.createElement("link");
-
-  dynamicStylesheet.setAttribute("href", FILE_URL);
-  dynamicStylesheet.setAttribute("type", "text/css");
-  dynamicStylesheet.setAttribute("rel", "stylesheet");
-
-  document.head.appendChild(dynamicStylesheet);
+    loadJS('/js/chart-1.js');
+    loadJS('/js/chart-2.js');
 }
