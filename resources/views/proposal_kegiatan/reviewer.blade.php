63fc7b6de9864be213d77a3d9ad4c30f4588c0fd
Tim1357912 <timothy.elroy.tif23@polban.ac.id>
2024-12-15 15:32:54 +0700

    perbaikan minor

--- resources/views/proposal_kegiatan/reviewer.blade.php
+++ resources/views/proposal_kegiatan/reviewer.blade.php
@@ -401,7 +401,7 @@
               </div>
               <span class="ml-1 duration-300 opacity-100 pointer-events-none ease-soft">Profil</span>
             </a>
-          </li> --}}
+          </li>
 
           {{-- <li class="mt-0.5 w-full">
             <a class="py-2.7 text-sm ease-nav-brand my-0 mx-4 flex items-center whitespace-nowrap px-4 transition-colors" href="/sign-in">
