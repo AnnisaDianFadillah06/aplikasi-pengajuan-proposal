c28ddb47cc049b8e8a23b069865b5730c7f4e0da
harishalrasyidi <alrasyidiharish@gmail.com>
2025-01-17 06:23:31 +0700

    perbaiki nama seeder

--- database/seeders/MahasiswaASLISeeder.php
+++ database/seeders/MahasiswaASLISeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class MahasiswaSeeder extends Seeder
+class MahasiswaASLISeeder extends Seeder
 {
     /**
      * Run the database seeds.
--- database/seeders/PengajuASLISeeder.php
+++ database/seeders/PengajuASLISeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class PengajuSeeder extends Seeder
+class PengajuASLISeeder extends Seeder
 {
     /**
      * Run the database seeds.
